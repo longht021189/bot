e4c691c75999ff4807653151fb15650973bf4f90
Thanh Long <longht021189@gmail.com>
2024-08-16 15:29:48 +0700

    add build yalantinglibs

---
+++ scripts/yalantinglibs/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+pushd $YALANTINGLIBS_DIR
+  cmake -H. \
+    -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARK=OFF -DBUILD_UNIT_TESTS=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -G Ninja -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -B$BUILD_DIR
+
+  cmake --build $BUILD_DIR --config Release --target all -j 6 -v
+  cmake --install $BUILD_DIR
+popd
+
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
+
+mv $INSTALL_DIR/lib $OUT_DIR/lib
+mv $INSTALL_DIR/include $OUT_DIR/include
